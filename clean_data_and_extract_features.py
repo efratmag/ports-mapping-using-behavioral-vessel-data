e6073fe291577b6a5366d19081854f04cc466562
Alon Cohen <alon@placer.io>
2021-03-01 11:50:09 +0200

    cleaning and extracing features from the raw data
    clean_data_and_extract_features

--- clean_data_and_extract_features.py
+++ clean_data_and_extract_features.py
@@ -1,5 +1,6 @@
 from shapely.geometry import Point
 
+from shapely import ops
 from area_snaptshot.area_snapshot import load_and_process_polygons_file, extract_coordinates
 
 import os
@@ -10,6 +11,8 @@ import geopandas as gpd
 
 import logging
 
+from geo_utils import geo_dist
+
 ACTIVITIES_FILES = ['mooring.csv.gz', 'drifting.csv.gz', 'port_calls.csv.gz', 'anchoring.csv.gz']
 
 VESSELS_RELEVANT_COLS = ['_id', 'class_calc', 'subclass_documented', 'built_year', 'name', 'deadweight', 'size', 'age']
@@ -19,24 +22,60 @@ def find_intersection_with_polygons(df, polygons_df, col_prefix):
 
     logging.debug(f'converting to Point - {col_prefix}...')
 
-    df['geometry'] = df.apply(lambda x: Point(x[col_prefix + '_lng'], x[col_prefix + '_lat']) if not pd.isna(
-        x[col_prefix + '_lat']) else None, axis=1)
+    if col_prefix+'_polygon_id' not in df.columns:
+
+        df['geometry'] = df.apply(lambda x: Point(x[col_prefix + '_lng'], x[col_prefix + '_lat']) if not pd.isna(
+            x[col_prefix + '_lat']) else None, axis=1)
+
+        df = gpd.GeoDataFrame(df)
+
+        logging.debug(f'performing spatial join - {col_prefix}...')
+        df = gpd.sjoin(df, polygons_df[['polygon_id', 'geometry']], how='left').drop('index_right', axis=1)
+
+        df['polygon_id'] = df['polygon_id'].astype(str)
+
+        logging.debug(f'aggregating polygons - {col_prefix}...')
+        merged_polygons = df.groupby('_id', as_index=False).agg({'polygon_id': ', '.join}).replace({'nan': None})
+
+        df = df.drop_duplicates('_id').drop('polygon_id', axis=1)
+        df = df.merge(merged_polygons, on='_id')
+        df = df.rename(columns={'polygon_id': col_prefix + '_polygon_id'})
+
+    return df
+
+
+def calc_distance_from_shore(df, shore_lines, col="firstBlip"):
+
+    logging.debug(f'Calc Distance From Shore - {col}...')
+
+    if col+'_closer_shore_lat' not in df.columns:
+
+        df['geometry'] = df.apply(lambda x: Point(x[col + '_lng'], x[col + '_lat']) if not pd.isna(
+            x[col + '_lat']) else None, axis=1)
 
     df = gpd.GeoDataFrame(df)
 
-    logging.debug(f'performing spatial join - {col_prefix}...')
-    df = gpd.sjoin(df, polygons_df[['polygon_id', 'geometry']], how='left').drop('index_right', axis=1)
+    df['nearest_shore_point'] = df.apply(lambda x: ops.nearest_points(shore_lines, x['geometry'])[0], axis=1)
+    df[col+'_nearest_shore_lng'] = df['nearest_shore_point'].apply(lambda x: x.x)
+    df[col+'_nearest_shore_lat'] = df['nearest_shore_point'].apply(lambda x: x.y)
 
-    df['polygon_id'] = df['polygon_id'].astype(str)
+    df = df.drop('nearest_shore_point', axis=1)
 
-    logging.debug(f'aggregating polygons - {col_prefix}...')
-    merged_polygons = df.groupby('_id', as_index=False).agg({'polygon_id': ', '.join}).replace({'nan': None})
-
-    df = df.drop_duplicates('_id').drop('polygon_id', axis=1)
-    df = df.merge(merged_polygons, on='_id')
-    df = df.rename(columns={'polygon_id': col_prefix + '_polygon_id'})
+    df[col+'_distance_from_shore'] = df.apply(lambda x: geo_dist(x[col + '_lat'],
+                                                                 x[col + '_lng'],
+                                                                 x[col+'_nearest_shore_lat'],
+                                                                 x[col+'_nearest_shore_lng']), axis=1)
 
     return df
+
+
+def load_and_process_shorelines_df(shorelines_file_path):
+
+    logging.info(f'loading file shorelines...')
+    shore_lines_df = gpd.read_file(shorelines_file_path)
+    shore_lines = ops.linemerge(shore_lines_df['geometry'].values)
+
+    return shore_lines
 
 
 def main(import_path, export_path, debug=True):
@@ -56,6 +95,10 @@ def main(import_path, export_path, debug=True):
     polygons_file_path = os.path.join(import_path, 'polygons.json')
     polygons_df = load_and_process_polygons_file(polygons_file_path)
 
+    shorelines_file_path = os.path.join(import_path, 'shoreline_layer_merged.geojson')
+
+    shore_lines = load_and_process_shorelines_df(shorelines_file_path)
+
     for file_name in files_list:
 
         file_path = os.path.join(import_path, file_name)
@@ -69,10 +112,13 @@ def main(import_path, export_path, debug=True):
             df = find_intersection_with_polygons(df, polygons_df, 'firstBlip')
             df = find_intersection_with_polygons(df, polygons_df, 'lastBlip')
 
+            df = calc_distance_from_shore(df, shore_lines, 'firstBlip')
+            df = calc_distance_from_shore(df, shore_lines, 'lastBlip')
+
             df.to_csv(os.path.join(export_path, file_name), index=False, compression='gzip')
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     fire.Fire(main)
 
