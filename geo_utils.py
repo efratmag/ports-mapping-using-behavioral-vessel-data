20d415572212a438a8a401110a0d34a1d1ac31b5
Alon Cohen <alon@placer.io>
2021-03-01 11:49:15 +0200

    Update geo_utils.py
    geo_dist added to geo_utils

--- geo_utils.py
+++ geo_utils.py
@@ -1,9 +1,9 @@
 import math
 
-R = 6378.1  # Radius of the Earth
+R_KM = 6378.1  # Radius of the Earth
 brng_n_e = 1.0472  # 60 degrees converted to radians.
 brng_s_w = 4.18879  # 240 degrees converted to radians.
-
+AVG_EARTH_RADIUS_M = 6371000.0
 
 def calc_dest_point(lat, lng, brng, d=15):
     """
@@ -20,11 +20,11 @@ def calc_dest_point(lat, lng, brng, d=15):
     lat = math.radians(lat)
     lng = math.radians(lng)
 
-    dest_lat = math.asin(math.sin(lat) * math.cos(d / R) +
-                     math.cos(lat) * math.sin(d / R) * math.cos(brng))
+    dest_lat = math.asin(math.sin(lat) * math.cos(d / R_KM) +
+                         math.cos(lat) * math.sin(d / R_KM) * math.cos(brng))
 
-    dest_lng = lng + math.atan2(math.sin(brng) * math.sin(d / R) * math.cos(lat),
-                             math.cos(d / R) - math.sin(lat) * math.sin(dest_lat))
+    dest_lng = lng + math.atan2(math.sin(brng) * math.sin(d / R_KM) * math.cos(lat),
+                                math.cos(d / R_KM) - math.sin(lat) * math.sin(dest_lat))
 
     dest_lat = math.degrees(dest_lat)
     dest_lng = math.degrees(dest_lng)
@@ -68,3 +68,23 @@ def isin_box(lat, lng, bounds):
             within = True
 
     return within
+
+
+def geo_dist(lat1, lng1, lat2, lng2):
+    """
+    Return distance in meters between the two points calculated with haversine formula
+    """
+    # convert decimal degrees to radians
+    lat_1 = math.radians(lat1)
+    lng_1 = math.radians(lng1)
+    lat_2 = math.radians(lat2)
+    lng_2 = math.radians(lng2)
+
+    # haversine formula
+    dlon = lng_2 - lng_1
+    dlat = lat_2 - lat_1
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat_1) * math.cos(lat_2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.asin(math.sqrt(a))
+    dist_mr = AVG_EARTH_RADIUS_M * c
+
+    return dist_mr
