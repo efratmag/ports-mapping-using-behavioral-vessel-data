d17eadf10fe92a7e79bd28717845c4d53696156e
efratmag <71638577+efratmag@users.noreply.github.com>
2021-03-09 10:01:29 +0200

    added abir's alpha_shape algo

--- geo_utils.py
+++ geo_utils.py
@@ -1,6 +1,10 @@
 import math
 import json
-from shapely.geometry import shape, Point
+from shapely.geometry import shape, Point, geometry
+from scipy.spatial import ConvexHull, Delaunay
+import numpy as np
+from shapely.ops import cascaded_union, polygonize
+
 
 R = 6378.1  # Radius of the Earth
 brng_n_e = 1.0472  # 60 degrees converted to radians.
@@ -104,4 +108,65 @@ def is_in_polygon_features(df):
     df['lastBlip_in_polygon'] = np.select(conditions, choices)
 
     return df
+
+
+def alpha_shape(points, alpha, only_outer=True):
+    # if len(points) < 4:
+    #     # When you have a triangle, there is no sense
+    #     # in computing an alpha shape.
+    #     return geometry.MultiPoint(list(points)).convex_hull
+
+    def add_edge(edges, edge_points, coords, i, j):
+        """
+        Add a line between the i-th and j-th points,
+        if not in the list already
+        """
+        if (i, j) in edges or (j, i) in edges:
+            if only_outer:
+                # if both neighboring triangles are in shape, it's not a boundary edge
+                edges.remove((j, i))
+            # already added
+            return
+        edges.add((i, j))
+        edge_points.append(coords[[i, j]])
+
+    coords = points  # np.array([point.coords[0] for point in points])
+    tri = Delaunay(coords)
+    edges = set()
+    edge_points = []
+    # loop over triangles:
+    # ia, ib, ic = indices of corner points of the
+    # triangle
+    for ia, ib, ic in tri.vertices:
+        pa = coords[ia]
+        pb = coords[ib]
+        pc = coords[ic]
+
+        # Lengths of sides of triangle
+        a = math.sqrt((pa[0] - pb[0]) ** 2 + (pa[1] - pb[1]) ** 2)
+        b = math.sqrt((pb[0] - pc[0]) ** 2 + (pb[1] - pc[1]) ** 2)
+        c = math.sqrt((pc[0] - pa[0]) ** 2 + (pc[1] - pa[1]) ** 2)
+
+        # Semiperimeter of triangle
+        s = (a + b + c) / 2.0
+
+        # Area of triangle by Heron's formula
+        # area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+        # circum_r = a*b*c/(4.0*area)
+        area = math.sqrt(abs(s * (s - a) * (s - b) * (s - c)))
+        if area != 0:
+            circum_r = a * b * c / (4.0 * area)
+        else:
+            circum_r = np.Inf
+
+        # Here's the radius filter.
+        # print(circum_r)
+        if circum_r < 1.0 / alpha:
+            add_edge(edges, edge_points, coords, ia, ib)
+            add_edge(edges, edge_points, coords, ib, ic)
+            add_edge(edges, edge_points, coords, ic, ia)
+
+    m = geometry.MultiLineString(edge_points)
+    triangles = list(polygonize(m))
+    return cascaded_union(triangles), edge_points, edges
 
