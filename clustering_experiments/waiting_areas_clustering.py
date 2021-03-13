a5477caf69420a799f6009110c5e100f38f7067e
Alon Cohen <alon@placer.io>
2021-03-13 18:28:11 +0200

    Update waiting_areas_clustering.py
    df renaming

--- clustering_experiments/waiting_areas_clustering.py
+++ clustering_experiments/waiting_areas_clustering.py
@@ -59,27 +59,27 @@ def main(import_path, export_path, col='firstBlip', dbscan_eps=2, dbscan_min_sam
     df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df[col+'_lng'],
                                                                        df[col+'_lat']))
 
-    anchoring_df = df[(df['activity'] == 'anchoring') & (df['vessel_class_calc'].isin(['Tanker', 'Cargo']))]
+    filtered_df = df[(df['activity'] == 'anchoring') & (df['vessel_class_calc'].isin(['Tanker', 'Cargo']))]
 
-    avg_lat = anchoring_df[col+'_lat'].mean()
-    avg_lng = anchoring_df[col + '_lng'].mean()
+    avg_lat = filtered_df[col+'_lat'].mean()
+    avg_lng = filtered_df[col + '_lng'].mean()
 
     #  define the map center by the given coordinates
     MAP_CONFIG_CLUSTERING['config']['mapState']['latitude'] = avg_lat
     MAP_CONFIG_CLUSTERING['config']['mapState']['longitude'] = avg_lng
 
     logging.info(f'performing clustering, total points {len(df)}...')
-    blips_distance_matrix = squareform(pdist(anchoring_df[['firstBlip_lat', 'firstBlip_lng']], (lambda u, v: haversine(u, v))))
+    blips_distance_matrix = squareform(pdist(filtered_df[['firstBlip_lat', 'firstBlip_lng']], (lambda u, v: haversine(u, v))))
     dbscan = DBSCAN(eps=dbscan_eps, min_samples=dbscan_min_samples, metric="precomputed").fit(blips_distance_matrix)
 
-    anchoring_df['labels'] = dbscan.labels_
-    anchoring_df['labels'] = 'cluster_' + anchoring_df['labels'].astype(str)
+    filtered_df['labels'] = dbscan.labels_
+    filtered_df['labels'] = 'cluster_' + filtered_df['labels'].astype(str)
 
     poly_list = []
 
     logging.info(f'generating polygons...')
-    for label in anchoring_df[(anchoring_df['labels'] != 'cluster_-1')]['labels'].unique():
-        cluster_df = anchoring_df[anchoring_df['labels'] == label]
+    for label in filtered_df[(filtered_df['labels'] != 'cluster_-1')]['labels'].unique():
+        cluster_df = filtered_df[filtered_df['labels'] == label]
         cluster_df = remove_outliers(cluster_df, outliers_std, col)
 
         poly, _, _ = alpha_shape(cluster_df[[col+'_lng', col+'_lat']].to_numpy(), alpha=alpha)
@@ -87,10 +87,10 @@ def main(import_path, export_path, col='firstBlip', dbscan_eps=2, dbscan_min_sam
 
     polygons_df = gpd.GeoDataFrame(poly_list)
 
-    anchoring_df.to_csv(os.path.join(export_path, clustering_file_name), index=False)
+    filtered_df.to_csv(os.path.join(export_path, clustering_file_name), index=False)
     polygons_df.to_file(os.path.join(export_path, f'{polyons_file_name}.geojson'), driver='GeoJSON')
 
-    kepler_map.add_data(anchoring_df, 'anchoring_clusters')
+    kepler_map.add_data(filtered_df, 'anchoring_clusters')
     kepler_map.add_data(df, 'all_activities')
     kepler_map.add_data(polygons_df, 'polygons')
     kepler_map.save_to_html(file_name=os.path.join(export_path, map_file_name), config=MAP_CONFIG_CLUSTERING)
