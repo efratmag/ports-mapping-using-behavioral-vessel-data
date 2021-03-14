58deb3a68dacb74209a7667620a480c8c4777769
Alon Cohen <alon@placer.io>
2021-03-15 01:15:25 +0200

    Update waiting_areas_clustering.py
    ports option added to code

--- clustering_experiments/waiting_areas_clustering.py
+++ clustering_experiments/waiting_areas_clustering.py
@@ -1,6 +1,6 @@
 from pyports.area_snaptshot.area_snapshot import today_str
 from pyports.area_snaptshot.map_configs import MAP_CONFIG_CLUSTERING
-from pyports.geo_utils import haversine, alpha_shape
+from pyports.geo_utils import haversine, alpha_shape, calc_polygon_area_sqmi
 from scipy.spatial.distance import pdist, squareform
 from scipy.spatial import distance_matrix
 from sklearn.cluster import DBSCAN
@@ -32,12 +32,21 @@ def remove_outliers(df, std=3.5, col='firstBlip'):
     return df
 
 
-def main(import_path, export_path, col='firstBlip', dbscan_eps=2, dbscan_min_samples=10, outliers_std=3.5, alpha=2):
+def points_to_polygon(df, outliers_std, col, alpha):
+
+    df = remove_outliers(df, outliers_std, col)
+    poly, _, _ = alpha_shape(df[[col + '_lng', col + '_lat']].to_numpy(), alpha=alpha)
+
+    return poly
+
+
+def main(import_path, export_path, ports=False, col='firstBlip', dbscan_eps=2, dbscan_min_samples=10, outliers_std=3.5, alpha=2):
 
     """
 
     :param import_path:
     :param export_path:
+    :param ports: if True, will generate polygons to ports as well as waiting areas
     :param col: FirstBlip / LastBlip
     :param dbscan_eps:
     :param dbscan_min_samples:
@@ -49,7 +58,7 @@ def main(import_path, export_path, col='firstBlip', dbscan_eps=2, dbscan_min_sam
     map_file_name = f'clustering_map_{today_str()}.html'
 
     clustering_file_name = f'clustering_points_{today_str()}.csv'
-    polyons_file_name = f'clustering_polygons_{today_str()}.csv'
+    polygons_file_name = f'clustering_polygons_{today_str()}.geojson'
 
     kepler_map = KeplerGl()
 
@@ -57,40 +66,65 @@ def main(import_path, export_path, col='firstBlip', dbscan_eps=2, dbscan_min_sam
     df = pd.read_csv(import_path)
 
     df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df[col+'_lng'],
-                                                                       df[col+'_lat']))
+                                                          df[col+'_lat']))
 
-    filtered_df = df[(df['activity'] == 'anchoring') & (df['vessel_class_calc'].isin(['Tanker', 'Cargo']))]
+    activities = ['anchoring']
 
-    avg_lat = filtered_df[col+'_lat'].mean()
-    avg_lng = filtered_df[col + '_lng'].mean()
+    if ports:
+        activities.append('mooring')
+
+    activities_df = df[(df['activity'].isin(activities)) & (df['vessel_class_calc'].isin(['Tanker', 'Cargo']))]
+
+    avg_lat = activities_df[col+'_lat'].mean()
+    avg_lng = activities_df[col + '_lng'].mean()
 
     #  define the map center by the given coordinates
     MAP_CONFIG_CLUSTERING['config']['mapState']['latitude'] = avg_lat
     MAP_CONFIG_CLUSTERING['config']['mapState']['longitude'] = avg_lng
 
-    logging.info(f'performing clustering, total points {len(df)}...')
-    blips_distance_matrix = squareform(pdist(filtered_df[['firstBlip_lat', 'firstBlip_lng']], (lambda u, v: haversine(u, v))))
+    logging.info(f'performing clustering, total points {len(activities_df)}...')
+    blips_distance_matrix = squareform(pdist(activities_df[[col+'_lat', col+'_lng']], (lambda u, v: haversine(u, v))))
     dbscan = DBSCAN(eps=dbscan_eps, min_samples=dbscan_min_samples, metric="precomputed").fit(blips_distance_matrix)
 
-    filtered_df['labels'] = dbscan.labels_
-    filtered_df['labels'] = 'cluster_' + filtered_df['labels'].astype(str)
+    activities_df['labels'] = dbscan.labels_
+    activities_df['labels'] = 'cluster_' + activities_df['labels'].astype(str)
 
     poly_list = []
 
     logging.info(f'generating polygons...')
-    for label in filtered_df[(filtered_df['labels'] != 'cluster_-1')]['labels'].unique():
-        cluster_df = filtered_df[filtered_df['labels'] == label]
-        cluster_df = remove_outliers(cluster_df, outliers_std, col)
+    for label in activities_df[(activities_df['labels'] != 'cluster_-1')]['labels'].unique():
 
-        poly, _, _ = alpha_shape(cluster_df[[col+'_lng', col+'_lat']].to_numpy(), alpha=alpha)
-        poly_list.append({'geometry': poly, 'label': label})
+        cluster_df = activities_df[activities_df['labels'] == label]
+
+        anchoring = cluster_df[(cluster_df['activity'] == 'anchoring')]
+        wa_poly = points_to_polygon(anchoring, outliers_std, col, alpha)
+        poly_list.append({'cluster': label,
+                          'type': 'Waiting Area',
+                          'geometry': wa_poly,
+                          'area_sqmi': calc_polygon_area_sqmi(wa_poly),
+                          'mean_duration (hours)': anchoring['duration'].mean()})
+
+        if ports:
+            try:
+                mooring = cluster_df[(cluster_df['activity'] == 'mooring')]
+                mooring = mooring[~mooring['geometry'].within(wa_poly)]
+                port_poly = points_to_polygon(mooring, 1.1, col, alpha)
+
+                poly_list.append({'cluster': label,
+                                  'type': 'Port',
+                                  'geometry': port_poly,
+                                  'area_sqmi': calc_polygon_area_sqmi(port_poly),
+                                  'mean_duration (hours)': mooring['duration'].mean()})
+
+            except Exception as e:
+                logging.info(f'failed to generate polygons for port {label}, reason - {e}')
 
     polygons_df = gpd.GeoDataFrame(poly_list)
 
-    filtered_df.to_csv(os.path.join(export_path, clustering_file_name), index=False)
-    polygons_df.to_file(os.path.join(export_path, f'{polyons_file_name}.geojson'), driver='GeoJSON')
+    activities_df.to_csv(os.path.join(export_path, clustering_file_name), index=False)
+    polygons_df.to_file(os.path.join(export_path, polygons_file_name), driver='GeoJSON')
 
-    kepler_map.add_data(filtered_df, 'anchoring_clusters')
+    kepler_map.add_data(activities_df, 'clusters')
     kepler_map.add_data(df, 'all_activities')
     kepler_map.add_data(polygons_df, 'polygons')
     kepler_map.save_to_html(file_name=os.path.join(export_path, map_file_name), config=MAP_CONFIG_CLUSTERING)
