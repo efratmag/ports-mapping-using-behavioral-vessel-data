d8c057607bf2b249d48da0ddd0ff82b31069b903
efratmag <71638577+efratmag@users.noreply.github.com>
2021-03-24 20:23:50 +0200

    minor fix

--- pyports/hdbscan_baseline_model.py
+++ pyports/hdbscan_baseline_model.py
@@ -59,7 +59,7 @@ def polygenize_clusters_with_features(df_for_clustering, polygons_df, ports_df, 
     return clust_polygons
 
 
-def main(path, df_for_clustering_fname, hdbscan_min_cluster_zise=15, hdbscan_min_samples=1, polygon_fname=None):
+def main(path=PATH, df_for_clustering_fname=FILE_NAME, hdbscan_min_cluster_zise=30, hdbscan_min_samples=5, polygon_fname=None):
 
     # import df and clean it
     df = pd.read_csv(os.path.join(path, df_for_clustering_fname))
