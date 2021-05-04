8ae1be886b6df8467d659118b19b43f0aa41e3ef
Alon Cohen <alon@placer.io>
2021-05-04 09:52:56 +0300

    setup files
    setup files - first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='cluster-activities',
+      version='0.1',
+      description='Ports mapping using behavioral vessel data',
+      url='http://github.com/storborg/funniest',
+      author='Efrat Magidov, PhD & Alon Cohen',
+      author_email='efratmag@gmail.com, bigalon1990@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
