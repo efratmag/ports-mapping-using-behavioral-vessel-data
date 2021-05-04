c14d8c796d833ed16ea2f33c660316a48a97561a
Alon Cohen <alon@placer.io>
2021-05-04 15:40:35 +0300

    Update setup.py
    setup file updating

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_packages
 setup(name='cluster-activities',
       version='0.1',
       description='Ports mapping using behavioral vessel data',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/efratmag/ports-mapping-using-behavioral-vessel-data',
       author='Efrat Magidov, PhD & Alon Cohen',
       author_email='efratmag@gmail.com, bigalon1990@gmail.com',
-      license='MIT',
       packages=find_packages(),
       zip_safe=False)
