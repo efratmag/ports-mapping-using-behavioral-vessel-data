aacdeec9998d7c5ecc0a9eef23ae9bce0a13a1fe
Alon Cohen <alon@placer.io>
2021-06-09 01:09:23 +0300

    Update get_activity_data.py
    get activity data - changes after Abir's review

--- pyports/get_activity_data.py
+++ pyports/get_activity_data.py
@@ -2,242 +2,98 @@
 Get activity data from database/ import_path and extract needed features.
 """
 
-from shapely.geometry import shape, Point
+from shapely.geometry import Point
+from enum import Enum
 import os
 import fire
 import pandas as pd
-import numpy as np
 import geopandas as gpd
 import pymongo
 from bson import ObjectId
-import json
 import logging
+from typing import List
+
+from pyports.get_data import get_vessels_info, get_ww_polygons
 
 
-# TODO: maybe enter parameter of type_of_area_mapped (ports vs pwa) to create df specific for each mapping
-
-class ACTIVITY(object):
-    """ constant parameter for activity type"""
+class ACTIVITY(Enum):
+    """constant parameter for activity type"""
     MOORING = 'mooring'
     ANCHORING = 'anchoring'
 
 
-def extract_coordinates(df, col='firstBlip'):
+def extract_coordinates(df: pd.DataFrame, blip: str) -> pd.DataFrame:
 
     """
     a function that extracts lat and lng from a Series with geometry dict
     :param df: Dataframe with coordinates dict columns
-    :param col: name of column for coordinates extraction
+    :param blip: "firstBlip" / "lastBlip"
     :return: df with lat lng coordinates
     """
 
-    logging.info('extract_coordinates - START')
+    coordinates_df = df[blip].dropna().apply(eval).apply(lambda x: x['geometry']['coordinates']).apply(pd.Series)
+    coordinates_df = coordinates_df.rename(columns={0: blip + '_lng', 1: blip + '_lat'})
 
-    if col+'_lng' not in df.columns and col+'_lat' not in df.columns:
-
-        logging.info(f'extracting coordinates for {col}...')
-        coordinates_df = df[col].dropna().apply(eval).apply(lambda x: x['geometry']['coordinates']).apply(pd.Series)
-        coordinates_df = coordinates_df.rename(columns={0: col+'_lng', 1: col+'_lat'})
-
-        df = df.merge(coordinates_df, left_index=True, right_index=True, how='left')
-
-    logging.info('extract_coordinates - END')
+    df = df.merge(coordinates_df, left_index=True, right_index=True, how='left')
 
     return df
 
 
-def find_intersection_with_polygons(df, polygons_df, col_prefix, force_enrichment=True):
+def find_intersection_with_polygons(df: pd.DataFrame, polygons_df: gpd.GeoDataFrame, blip: str) -> gpd.GeoDataFrame:
 
     """
     this function will find for each activity point its ww polygon id and type
     :param df: activity df
     :param polygons_df: ww polygons df
-    :param col_prefix: "firstBlip" / "lastBlip"
-    :param force_enrichment: if True, will recreate column, even if exists
+    :param blip: "firstBlip" / "lastBlip"
     :return: activity df with additional columns for ww polygon id and type
     """
 
-    logging.info(f'converting to Point - {col_prefix}...')
+    df['geometry'] = df.apply(lambda x: Point(x[blip + '_lng'], x[blip + '_lat']) if not pd.isna(
+        x[blip + '_lat']) else None, axis=1)  # convert lat,lng to Point object
 
-    if col_prefix+'_polygon_id' not in df.columns or force_enrichment:
+    df = gpd.GeoDataFrame(df)
 
-        df['geometry'] = df.apply(lambda x: Point(x[col_prefix + '_lng'], x[col_prefix + '_lat']) if not pd.isna(
-            x[col_prefix + '_lat']) else None, axis=1)  # convert lat,lng to Point object
+    df = gpd.sjoin(df, polygons_df[['polygon_id', 'polygon_area_type', 'geometry'
+                                    ]], how='left').drop('index_right', axis=1)  # spatial join
 
-        df = gpd.GeoDataFrame(df)
+    df['polygon_id'], df['polygon_area_type'] = df['polygon_id'].astype(str), df['polygon_area_type'].astype(str)
 
-        logging.info(f'performing spatial join - {col_prefix}...')
-        df = gpd.sjoin(df, polygons_df[['polygon_id', 'polygon_area_type', 'geometry'
-                                        ]], how='left').drop('index_right', axis=1)  # spatial join
+    merged_polygons = df.groupby('_id', as_index=False).agg({'polygon_id': ', '.join,
+                                                             'polygon_area_type': ', '.join}).replace({'nan': None})
 
-        df['polygon_id'], df['polygon_area_type'] = df['polygon_id'].astype(str), df['polygon_area_type'].astype(str)
-
-        logging.info(f'aggregating polygons - {col_prefix}...')
-        merged_polygons = df.groupby('_id', as_index=False).agg({'polygon_id': ', '.join,
-                                                                 'polygon_area_type': ', '.join}).replace({'nan': None})
-
-        df = df.drop_duplicates('_id').drop(['polygon_id', 'polygon_area_type', 'geometry'], axis=1)
-        df = df.merge(merged_polygons, on='_id')
-        df = df.rename(columns={'polygon_id': col_prefix + '_polygon_id',
-                                'polygon_area_type': col_prefix + '_polygon_area_type'})
+    df = df.drop_duplicates('_id').drop(['polygon_id', 'polygon_area_type', 'geometry'], axis=1)
+    df = df.merge(merged_polygons, on='_id')
+    df = df.rename(columns={'polygon_id': blip + '_polygon_id',
+                            'polygon_area_type': blip + '_polygon_area_type'})
 
     return df
 
 
-def get_ww_polygons(import_path=None, db=None):
+def get_activity_df(import_path: str, db: pymongo.MongoClient, vessels_ids: List[str] = None,
+                    activity: ACTIVITY = ACTIVITY.MOORING, debug: bool = False) -> pd.DataFrame:
 
     """
-    function that extract ports & waiting areas polygons
-    :param import_path: path to the ports & waiting areas polygons file location
-    :param db: MongoDB object
-    :return:
-    """
-
-    logging.info('get_ports_wa_polygons - START')
-    # TODO: move to new file (get_data)
-    # TODO: fill in mongo query
-    if not import_path:
-        col = db["polygons"]
-
-        col = col.find({'properties.areaType': {'$in': ['Port', 'PortWaitingArea']}},
-                       {'_id': 1, 'properties.title':  1, 'geometry': 1})
-
-        polygons_df = pd.DataFrame(list(col))
-        polygons_df = polygons_df.rename(columns={'properties.areaType': 'areaType', 'properties.title': 'title'})
-
-    else:
-        polygons_file_path = os.path.join(import_path, 'polygons.json')
-        with open(polygons_file_path, 'r') as polygons_file:
-            polygons_file = json.load(polygons_file)
-        polygons_df = pd.DataFrame.from_dict(polygons_file, orient='index')
-        polygons_df['_id'] = polygons_df.index
-        polygons_df.reset_index(drop=True, inplace=True)
-        polygons_df['title'] = [d.get('title') for d in polygons_df.properties]  # add name column from nested dict
-        polygons_df['areaType'] = [d.get('areaType') for d in
-                                   polygons_df.properties]  # add areaType column from nested dict
-        polygons_df = polygons_df[['_id', 'geometry', 'title', 'areaType']]
-
-    polygons_df = polygons_df.rename(columns={'_id': 'polygon_id',
-                                              'areaType': 'polygon_area_type',
-                                              'title': 'name'})
-    polygons_df['geometry'] = polygons_df['geometry'].apply(shape)  # convert to shape object
-
-    polygons_df = gpd.GeoDataFrame(polygons_df)  # convert to GeoPandas
-
-    logging.info('get_ports_wa_polygons - END')
-
-    return polygons_df
-
-
-def get_vessels_info(import_path, db, vessels_ids=None):
-
-    """
-    function that extract vessels info
-    :param import_path: path to the vessels info file location
-    :param db: MongoDB object
-    :param vessels_ids: filter data by specific vessels_ids
-    :return:
-    """
-
-    logging.info('get_vessels_info - START')
-
-    projection = {'vesselId': 1, "class_calc": 1, "subclass_documented": 1, "built_year": 1,
-                  "name": 1, "deadweight": 1, "draught": 1, "size": 1, "age": 1}
-
-    # TODO: fill in mongo query
-    if not import_path:
-        col = db["vessels"]
-        col = col.find({'_id': {'$in': [ObjectId(vessel) for vessel in vessels_ids]}} if vessels_ids else {}, projection)
-
-        vessels_df = pd.DataFrame(list(col)).drop(['_id'], axis=1)
-
-    else:
-
-        vessels_file_path = os.path.join(import_path, 'vessels.json')
-        with open(vessels_file_path, 'r') as vessels_file:
-            vessels_file = json.load(vessels_file)
-            vessels_df = pd.DataFrame.from_dict(vessels_file, orient='index')
-
-            if vessels_ids:
-                vessels_df = vessels_df[vessels_df.index.isin(vessels_ids)]
-
-            vessels_df = vessels_df[projection].drop('vesselId', axis=1)
-
-    conditions = [(vessels_df["class_calc"] == 'Cargo') & (vessels_df["subclass_documented"] == 'Container Vessel'),
-                  (vessels_df["class_calc"] == 'Cargo') & (vessels_df["subclass_documented"] != 'Container Vessel'),
-                  (vessels_df["class_calc"] == 'Tanker')]
-
-    choices = ["cargo_container", "cargo_other", "tanker"]
-    vessels_df["class_new"] = np.select(conditions, choices)
-    vessels_df["class_new"] = vessels_df["class_new"].replace({'0': 'other'})
-
-    vessels_df = vessels_df.add_prefix('vessel_')
-    vessels_df = vessels_df.reset_index().rename(columns={'index': 'vesselId'})
-
-    logging.info('get_vessels_info - END')
-
-    return vessels_df
-
-
-def get_ports_info(import_path, db):
-
-    """
-    function that extract ports info
-    :param import_path: path to the ports info file location
-    :param db: MongoDB object
-    :return:
-    """
-
-    logging.info('get_ports_info - START')
-
-    # TODO: fill in mongo query
-    if not import_path:
-        col = db["ports"]
-        col = col.find({}, {'_id': 1, 'country': 1, 'name': 1, 'center_coordinates.0': 1, 'center_coordinates.1': 1})
-        ports_df = pd.DataFrame(list(col)).drop(['_id'], axis=1)
-        ports_df = ports_df.rename(columns={'center_coordinates.0': 'lng', 'center_coordinates.1': 'lat'})
-
-    else:
-        ports_file_path = os.path.join(import_path, 'ports.json')
-        with open(ports_file_path, 'r') as portsfile:
-
-            ports_df = json.load(portsfile)
-        ports_df = pd.DataFrame.from_dict(ports_df, orient='index')
-        ports_df = ports_df[['country', 'name', 'center_coordinates']]
-
-        ports_df['lat'] = ports_df.center_coordinates.map(lambda x: x[1])
-        ports_df['lng'] = ports_df.center_coordinates.map(lambda x: x[0])
-
-        ports_df = ports_df.reset_index().rename(columns={'index': 'PortId'}).drop('center_coordinates', axis=1)
-
-        logging.info('get_ports_info - END')
-
-    return ports_df
-
-
-def get_activity_df(import_path, db, vessels_ids=None, activity=ACTIVITY.MOORING, nrows=None):
-
-    """
-
+    this function will load the activity data and if needed, will extract features
     :param import_path:
     :param db: MongoDB object
     :param vessels_ids: filter data by specific vessels_ids
     :param activity: "mooring" / "anchoring"
-    :param nrows: if passed, will limit the df by this value
+    :param debug: if True, only first 10K rows of activity df will be loaded
     :return:
     """
 
-    logging.info(F'get_activity_df ({activity}) - START')
+    nrows = 10000 if debug else None
 
     # TODO: fill in mongo query
     if not import_path:
         query = {'vesselId': {'$in': [ObjectId(vessel) for vessel in vessels_ids]}} if vessels_ids else {}
-        projection = {'_id': 1, 'vesselId': 1, 'startDate': 1, 'endDate': 1, 'duration': 1, 'nextPort': 1,
-                      'firstBlip.geometry.coordinates.0': 1, 'firstBlip.geometry.coordinates.1': 1,
-                      'lastBlip.geometry.coordinates.0': 1, 'lastBlip.geometry.coordinates.1': 1}
+        columns_projection = {'_id': 1, 'vesselId': 1, 'startDate': 1, 'endDate': 1, 'duration': 1, 'nextPort': 1,
+                              'firstBlip.geometry.coordinates.0': 1, 'firstBlip.geometry.coordinates.1': 1,
+                              'lastBlip.geometry.coordinates.0': 1, 'lastBlip.geometry.coordinates.1': 1}
 
-        col = db[activity]
-        col = col.find(query, projection).limit(nrows) if nrows else col.find(query, projection)
+        col = db[activity.value]
+        col = col.find(query, columns_projection).limit(nrows) if nrows else col.find(query, columns_projection)
         activity_df = pd.DataFrame(list(col))
         activity_df = activity_df.rename(columns={'firstBlip.geometry.coordinates.0': 'firstBlip_lng',
                                                   'firstBlip.geometry.coordinates.1': 'firstBlip_lat',
@@ -246,7 +102,7 @@ def get_activity_df(import_path, db, vessels_ids=None, activity=ACTIVITY.MOORING
 
     else:
         cols = ['_id', 'firstBlip', 'lastBlip', 'vesselId', 'startDate', 'endDate', 'duration', 'nextPort']
-        activity_file_path = os.path.join(import_path, f'{activity}.csv.gz')
+        activity_file_path = os.path.join(import_path, f'{activity.value}.csv.gz')
         activity_df = pd.read_csv(activity_file_path, nrows=nrows, usecols=cols)
 
         if vessels_ids:
@@ -256,16 +112,18 @@ def get_activity_df(import_path, db, vessels_ids=None, activity=ACTIVITY.MOORING
         activity_df = extract_coordinates(activity_df, 'lastBlip')
         activity_df = activity_df.drop(['firstBlip', 'lastBlip'], axis=1)  # drop nested locations columns
 
-    logging.info(f'get_activity_df ({activity}) - END')
+    logging.info(f'{activity.value} data extracted')
 
     return activity_df
 
 
-def main(export_path, vessels_ids=None, import_path=None, use_db=False, debug=True):
+def main(export_path: str, activity_type: ACTIVITY = ACTIVITY.MOORING, vessels_ids: str = None,
+         import_path: str = None, use_db: bool = None, debug: bool = True):
 
     """
     This code will get all relevant data and prepare it for clustering
     :param export_path: path in which the output will be exported
+    :param activity_type: "mooring" / "anchoring"
     :param import_path: path to directory with all relevant files
     :param vessels_ids: comma-separated list of vessels ids for mongo query
     :param use_db: if True, will use mongo db to query data
@@ -275,7 +133,7 @@ def main(export_path, vessels_ids=None, import_path=None, use_db=False, debug=Tr
 
     assert use_db or import_path, "use_db or import_path wasn't passed"
 
-    if vessels_ids and isinstance(vessels_ids, str):
+    if vessels_ids:
         vessels_ids = vessels_ids.split(',')
 
     # TODO: update with winward db methods
@@ -284,33 +142,25 @@ def main(export_path, vessels_ids=None, import_path=None, use_db=False, debug=Tr
         myclient = pymongo.MongoClient("<your connection string here>")  # initiate MongoClient for mongo queries
         db = myclient["<DB name here>"]
 
-    nrows = 10000 if debug else None  # 10K rows per activity file if debug == True
-
     polygons_df = get_ww_polygons(import_path, db)
     vessels_df = get_vessels_info(import_path, db, vessels_ids)
 
-    results_dict = {ACTIVITY.ANCHORING: None, ACTIVITY.MOORING: None}
+    activity_df = get_activity_df(import_path, db, vessels_ids, activity_type, debug)
 
-    for activity in [ACTIVITY.ANCHORING, ACTIVITY.MOORING]:
+    activity_df = find_intersection_with_polygons(activity_df, polygons_df, 'firstBlip')
+    activity_df = find_intersection_with_polygons(activity_df, polygons_df, 'lastBlip')
 
-        activity_df = get_activity_df(import_path, db, vessels_ids, activity, nrows)
+    activity_df = activity_df.merge(vessels_df, on='vesselId', how='left')
 
-        activity_df = find_intersection_with_polygons(activity_df, polygons_df, 'firstBlip')
-        activity_df = find_intersection_with_polygons(activity_df, polygons_df, 'lastBlip')
+    activity_df = activity_df.merge(polygons_df.set_index('polygon_id').drop('geometry', axis=1),
+                                    left_on='nextPort', right_index=True, how='left').rename(
+        columns={'name': 'nextPort_name'})
 
-        logging.info(f'merging vessels data...')
-        activity_df = activity_df.merge(vessels_df, on='vesselId', how='left')
+    activity_df.nextPort_name.fillna('UNKNOWN', inplace=True)
 
-        logging.info(f'merging nextPort data...')
-        activity_df = activity_df.merge(polygons_df.set_index('polygon_id').drop('geometry', axis=1),
-                                        left_on='nextPort', right_index=True, how='left').rename(
-            columns={'name': 'nextPort_name'})
-        activity_df.nextPort_name.fillna('UNKNOWN', inplace=True)
+    activity_df.to_csv(os.path.join(export_path, f'{activity_type.value}.csv.gz'), index=False, compression='gzip')
 
-        results_dict[activity] = activity_df
-        activity_df.to_csv(os.path.join(export_path, f'{activity}.csv.gz'), index=False, compression='gzip')
-
-    return results_dict
+    return activity_df
 
 
 if __name__ == "__main__":
