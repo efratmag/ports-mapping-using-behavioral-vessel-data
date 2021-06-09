6a56ec30448c8f0bbe0069c6da8a242d555cfa4b
Alon Cohen <alon@placer.io>
2021-06-09 19:11:23 +0300

    Create constants.py
    constant file added

---
+++ pyports/constants.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class ACTIVITY(Enum):
+    """constant parameter for activity type"""
+    MOORING = 'mooring'
+    ANCHORING = 'anchoring'
+
+
+class VesselType(Enum):
+    """constant parameter for vessels type"""
+    CARGO_CONTAINER = "cargo_container"
+    CARGO_OTHER = "cargo_other"
+    TANKER = "tanker"
+
+
+class BLIP(Enum):
+    # todo - do we want to use blip as enum?
+    """constant parameter for first/last blip lat/lng"""
+    FirstBlipLat = ''
+    FirstBlipLng = ''
+    LastBlipLat = ''
+    LastBlipLng = ''
