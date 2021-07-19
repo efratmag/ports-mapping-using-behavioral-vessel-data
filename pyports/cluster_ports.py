c26a8c7eab42fc27b3fa973a3b30759ddc016c91
efratmag <71638577+efratmag@users.noreply.github.com>
2021-07-19 11:06:55 +0300

    moved all preprocessing steps to outside function

--- pyports/cluster_ports.py
+++ pyports/cluster_ports.py
@@ -12,7 +12,6 @@ Lastly polygons are created from these clusters and their features are extracted
 from scipy.spatial import cKDTree
 from pyports.cluster_activities_utils import *
 from pyports.connected_components_utils import *
-from pyports.geo_utils import is_in_river
 from pyports.constants import ACTIVITY, AreaType
 from typing import Union
 import time
@@ -24,7 +23,7 @@ tqdm.pandas()
 
 def main(import_path: pathlib.Path, export_path: pathlib.Path, activity: Union[ACTIVITY, str] = ACTIVITY.MOORING,
          blip: str = 'first', type_of_area_mapped: Union[AreaType, str] = AreaType.PORTS,
-         filter_river_points: bool = True, epsilon: str = 10000, only_container_vessels: bool = False,
+         filter_river_points: bool = True, epsilon: int = 10000, only_container_vessels: bool = False,
          polygon_type: str = 'alpha_shape', polygon_alpha: int = 4, optimize_polygon: bool = False,
          sub_area_polygon_fname: str = None, save_files: bool = False, debug: bool = False):
 
@@ -54,49 +53,13 @@ def main(import_path: pathlib.Path, export_path: pathlib.Path, activity: Union[A
 
     logging.info('preprocessing data...')
 
-    locations = df[[f'{blip}Blip_lat', f'{blip}Blip_lon']].rename({f'{blip}Blip_lat': 'lat', f'{blip}Blip_lon': 'lon'},
-                                                                  axis=1).reset_index(drop=True)
-
-# TODO: for all exists file inspections- make sure the file checked is per running time
-    # filter out points in rivers
-    if filter_river_points:
-        if not import_path.joinpath("river_mask_mooring.csv").exists():
-            river_mask = is_in_river(locations, main_land)
-            river_mask.to_csv(os.path.join(import_path, 'river_mask_mooring.csv'))
-        else:
-            river_mask = pd.read_csv(import_path.joinpath('river_mask_mooring.csv'))
-        locations = locations[np.invert(river_mask)]  # take only ports where in_river == False
-        print(f'removed {np.sum(river_mask)} points that lay in rivers ('
-              f'{np.sum(river_mask) / locations.shape[0] *100:.2f}% of the data).')
-
-    # get locations_utm - projections of lat lon to utm coordinates
-    if not import_path.joinpath("locations_utm.csv").exists():
-        locations_utm = locations.progress_apply(lambda row: get_utm(row.lat, row.lon), axis=1)
-        locations_utm.to_csv(import_path.joinpath("locations_utm.csv"), index=False)
-    else:
-        locations_utm = pd.read_csv(import_path.joinpath("locations_utm.csv"))
-
-    # get zone feature by combining utm number and letter
-    locations_utm["zone"] = locations_utm.apply(lambda row: f"{row.zone_number}{row.zone_letter}", axis=1)
-
-    # get borders- a boolean per location indicating if it occurs close to a utm zone border (<epsilon away from border)
-    if not import_path.joinpath("locations_utm_border.csv").exists():
-        border_statuses = locations_utm.progress_apply(lambda row: is_border(row.lat,
-                                                                             row.lon,
-                                                                             row.zone_number,
-                                                                             row.zone_letter, epsilon),
-                                                       axis=1)
-        locations_utm = locations_utm.join(border_statuses)
-        locations_utm.to_csv(import_path.joinpath("locations_utm_border.csv"), index=False)
-    else:
-        locations_utm = pd.read_csv(import_path.joinpath("locations_utm_border.csv"))
-
+    locations_preprocessed = preprocess_for_connected_components(import_path, df, blip, main_land, filter_river_points,
+                                                                 epsilon)
 
     logging.info('starting growing connected components...')
 
     # initializations
-
-    locs = locations_utm.copy()
+    locs = locations_preprocessed.copy()
     locs["component"] = -1  # initialize all points as unmarked (i.e noise by default)
     # get sub zones by dividing easting/northing by epsiloneshold
     locs["cell_x"] = (locs["easting"] / epsilon).astype(int)
@@ -116,9 +79,9 @@ def main(import_path: pathlib.Path, export_path: pathlib.Path, activity: Union[A
 
     for zone, zone_locations in tqdm(zone_grp, total=len(zone_grp.groups)):
         zn, zl = zone
-        zone_mask = (locations_utm.zone_number == zn) & (locations_utm.zone_letter == zl)
-        tree = cKDTree(locations_utm.loc[zone_mask, ["easting", "northing"]].values)
-        kdtrees[zone] = [locations_utm.loc[zone_mask].index, tree]
+        zone_mask = (locations_preprocessed.zone_number == zn) & (locations_preprocessed.zone_letter == zl)
+        tree = cKDTree(locations_preprocessed.loc[zone_mask, ["easting", "northing"]].values)
+        kdtrees[zone] = [locations_preprocessed.loc[zone_mask].index, tree]
 
     # growing connected components
     global_start_t = time.time()
